2a4dc6d8cd7dac8d8f44fbab96f92009baef479c
gallavinod <vgalla@buffalo.edu>
2014-12-12 06:14:21 -0500

    added converters

--- src/iclassifier/FrequentItemMiner.java
+++ src/iclassifier/FrequentItemMiner.java
@@ -7,16 +7,16 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.Set;
 
 public class FrequentItemMiner {
-	
+
 	public static ArrayList <BigInteger> transactionsList = new ArrayList <BigInteger> ();
-	public static HashSet <BigInteger> baseItemSet = new HashSet <BigInteger> ();
-	public static Map <Integer, HashSet <BigInteger>> frequentPatternMap = new HashMap <Integer, HashSet <BigInteger>> ();
-	public static int size = 1;
-	
+	public static HashMap <BigInteger,Integer> baseItemMap = new HashMap <BigInteger,Integer> ();
+	public static HashMap <Integer, HashMap <BigInteger,Integer>> frequentPatternMap = new HashMap <Integer, HashMap <BigInteger,Integer>> ();
+	//	public static HashMap <BigInteger,Integer> totalFrequentMap = new HashMap <BigInteger,Integer> ();
+	//	public static Map <Integer, Integer> countTracker = new HashMap <Integer, Integer> ();
+
 	public static int frequency (BigInteger pattern) {
 		int count = 0;
 		for (BigInteger transaction : transactionsList) {
@@ -28,35 +28,58 @@ public class FrequentItemMiner {
 	}
 
 	public static void main(String[] args) {
+		Long startTime = System.nanoTime();
 		BufferedReader dataReader = null;
 		BufferedReader baseItemReader = null;
-		int threshold = 0;
-		
-		
+		int threshold = 2000;
+
+
 		try {
-		//	dataReader = new BufferedReader(new FileReader(new File(args[0])));
+			//	dataReader = new BufferedReader(new FileReader(new File(args[0])));
 			dataReader = new BufferedReader(new FileReader(new File("mushroom-edible-biginteger")));
 			String dataLine = null;
-			
+
 			while ((dataLine = dataReader.readLine()) != null) {
 				transactionsList.add(new BigInteger(dataLine,10));
 			}
 			dataReader.close();
 			System.out.println(transactionsList.size());
-			
+
 			baseItemReader = new BufferedReader(new FileReader(new File("item-index-biginteger")));
 			String itemLine = null;
-			
+
 			while ((itemLine = baseItemReader.readLine()) != null) {
 				int itemFrequency = frequency(new BigInteger(itemLine,10));
 				if (itemFrequency >= threshold) {
-					System.out.println(new BigInteger(itemLine,10)+" "+itemFrequency);
-					baseItemSet.add(new BigInteger(itemLine,10));
+					baseItemMap.put(new BigInteger(itemLine,10),itemFrequency);
 				}
 			}
 			baseItemReader.close();
-			
-			frequentPatternMap.put(size, baseItemSet);
+			System.out.println(1+" : "+baseItemMap.size());
+			frequentPatternMap.put(1, baseItemMap);
+			//	totalFrequentMap.putAll(baseItemMap);
+			int baseSize = baseItemMap.size();
+			for (int i=1;i<baseSize; i++) {
+				Set <BigInteger> previousCandidateSet = frequentPatternMap.get(i).keySet();
+				HashMap <BigInteger,Integer> currentCandidateMap = new HashMap <BigInteger,Integer> ();
+				for (BigInteger previousCandidate : previousCandidateSet) {
+					for (BigInteger baseItem : baseItemMap.keySet()) {
+						BigInteger newCandidate = previousCandidate.or(baseItem);
+						if (!newCandidate.equals(previousCandidate)) {
+							int candidateFrequency = frequency(newCandidate);
+							if (candidateFrequency >= threshold) {
+								currentCandidateMap.put(newCandidate,candidateFrequency);
+							}
+						}
+					}
+				}
+				System.out.println((i+1)+" : "+currentCandidateMap.size());
+				frequentPatternMap.put(i+1, currentCandidateMap);
+			}
+			System.out.println(frequentPatternMap.size());
+
+			long endTime = System.nanoTime();
+			System.out.println("Took "+(endTime - startTime) + " ns");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
